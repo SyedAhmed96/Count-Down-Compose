cc2d6c1736361ec789977bca1cece5b099eed879
Syed Ahmedullah <syedahmedullah@m3tech.com.pk>
2024-12-05 13:33:50 +0500

    - Test failure run case - Failure report generation script added

--- app/src/test/java/com/ahmed/countdowncompose/CountDownTimerTest.kt
+++ app/src/test/java/com/ahmed/countdowncompose/CountDownTimerTest.kt
@@ -46,6 +46,6 @@ class CountDownTimerTest {
         val result = countDownTimer.countDown(0).toList()
 
         // Then: Verifying that the emitted sequence is [0]
-        assertEquals(listOf(1), result)
+        assertEquals(listOf(2), result)
     }
 }
