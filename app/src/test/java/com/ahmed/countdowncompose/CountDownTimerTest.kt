07ca226d87775e10a68bedd26be6c8bdcd1b155c
Syed Ahmedullah <syedahmedullah@m3tech.com.pk>
2024-12-05 13:37:21 +0500

    Updated jdk to resolve CI error

--- app/src/test/java/com/ahmed/countdowncompose/CountDownTimerTest.kt
+++ app/src/test/java/com/ahmed/countdowncompose/CountDownTimerTest.kt
@@ -46,6 +46,6 @@ class CountDownTimerTest {
         val result = countDownTimer.countDown(0).toList()
 
         // Then: Verifying that the emitted sequence is [0]
-        assertEquals(listOf(2), result)
+        assertEquals(listOf(0), result)
     }
 }
